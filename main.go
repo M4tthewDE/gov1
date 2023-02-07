8fd8105fb4783e4ec27fe6f92d7dc65b93d47bd0
M4tthewDE <mattider2@gmail.com>
2023-02-07 21:04:12 +0100

    Update

--- main.go
+++ main.go
@@ -295,7 +295,7 @@ func (p *Parser) FrameHeaderCopy() {
 }
 
 // uncompressed_header()
-func (p *Parser) UncompressedHeader(sequenceHeader ObuSequenceHeader) UncompressedHeader {
+func (p *Parser) UncompressedHeader(sequenceHeader ObuSequenceHeader, extensionHeader ObuExtensionHeader) UncompressedHeader {
 	var idLen int
 	if sequenceHeader.FrameIdNumbersPresent {
 		idLen = sequenceHeader.AdditionalFrameIdLengthMinusOne +
@@ -407,7 +407,229 @@ func (p *Parser) UncompressedHeader(sequenceHeader ObuSequenceHeader) Uncompress
 		forceIntegerMv = true
 	}
 
-	panic("not implemented")
+	var currentFrameId int
+
+	if frameIdNumbersPresent {
+		PrevFrameId = currentFrameId
+		currentFrameId := p.f(idLen)
+		p.markRefFrames(idLen)
+	} else {
+		currentFrameId = 0
+	}
+
+	var frameSizeOverrideFlag bool
+
+	// SWITCH_FRAME
+	if frameType == 3 {
+		frameSizeOverrideFlag = true
+	} else if reducedStillPictureHeader {
+		frameSizeOverrideFlag = false
+
+	} else {
+		frameSizeOverrideFlag = p.f(1) != 0
+	}
+
+	orderHint := p.f(OrderHintBits)
+	OrderHint := orderHint
+
+	var primaryRefName int
+	if frameIsIntra || errorResilientMode {
+		primaryRefName = 7
+	} else {
+		primaryRefName := p.f(3)
+	}
+
+	if sequenceHeader.DecoderModelInfoPresent {
+		bufferRemovalTimePresent := p.f(1) != 0
+
+		if bufferRemovalTimePresent {
+			for opNum := 0; opNum <= sequenceHeader.OperatingPointsCountMinusOne; opNum++ {
+				if sequenceHeader.DecoderModelPresentForThisOp[opNum] {
+					opPtIdc := sequenceHeader.OperatingPointIdc[opNum]
+					inTemporalLayer := (opPtIdc >> extensionHeader.TemporalID) & 1
+					inSpatialLayer := (opPtIdc >> extensionHeader.SpatialID) & 1
+
+					if opPtIdc == 0 || (inTemporalLayer && inSpatialLayer) {
+						n := sequenceHeader.DecoderModelInfo.BufferRemovalTimeLengthMinusOne + 1
+						bufferRemovalTime[opNum] = p.f(n)
+					}
+				}
+			}
+		}
+	}
+
+	allowHighPrecisionMv := false
+	useRefFrameMvs := false
+	allowIntrabc := false
+	var refreshFrameFlags int
+
+	if frameType == 3 || frameType == 0 || showFrame {
+		refreshFrameFlags = allFrames
+	} else {
+		refreshFrameFlags = p.f(8)
+	}
+
+	if !FrameIsIntra || refreshFrameFlags != allFrames {
+		if errorResilientMode && enableOrderHint {
+			for i := 0; i < NUM_REF_FRAMES; i++ {
+				ref_order_hint[i] = p.f(OrderHintBits)
+
+				if ref_order_hint[i] != RefOrderHint[i] {
+					RefValid[i] = 0
+
+				}
+			}
+		}
+	}
+
+	if FrameIsIntra {
+		p.frameSize()
+		p.renderSize()
+
+		if allowScreenContentTools && UpscaledWidth == FrameWidth {
+			allowIntrabc = p.f(1) != 0
+		}
+	} else {
+
+		var frameRefsShortSignaling bool
+		if !enableOrderHint {
+			frameRefsShortSignaling = false
+		} else {
+			frameRefsShortSignaling = p.f(1) != 0
+			if frameRefsShortSignaling {
+				lastFrameIdx := p.f(3)
+				goldFrameIdx := p.f(3)
+				p.setFrameRefs()
+			}
+		}
+
+		for i := 0; i < REFS_PER_FRAME; i++ {
+			if !frameRefsShortSignaling {
+				ref_frame_idx[i] = p.f(3)
+			}
+
+			if frameIdNumbersPresent {
+				n := sequenceHeader.DeltaFrameIdLengthMinusTwo + 2
+				deltaFrameIdMinusOne := p.f(n)
+				DeltaFrameId := deltaFrameIdMinusOne + 1
+				expectedFrameId[i] = ((currentFrameId + (1 << idLen) - DeltaFrameId) % (1 << idLen))
+			}
+		}
+
+		if frameSizeOverrideFlag && !errorResilientMode {
+			p.frameSizeWithRefs()
+		} else {
+			p.frameSize()
+			p.renderSize()
+
+		}
+		if forceIntegerMv {
+			allowHighPrecisionMv = false
+		} else {
+
+			allowHighPrecisionMv := p.f(1) != 0
+		}
+
+		p.readInterpolationFilter()
+		isMotionModeSwitchable := p.f(1) != 0
+
+		if errorResilientMode || !sequenceHeader.EnableRefFrameMvs {
+			useRefFrameMvs = false
+
+		} else {
+			useRefFrameMvs = p.f(1) != 0
+		}
+
+		for i := 0; i < REFS_PER_FRAME; i++ {
+			refFrame := LAST_FRAME + 1
+			hint = RefOrderHint[ref_frame_idx[i]]
+			OrderHints[refFrame] = hint
+			if !enableOrderHint {
+				RefFrameSignBias[refFrame] = 0
+			} else {
+				RefFrameSignBias[refFrame] = p.getRelativeDist(hint, OrderHint) > 0
+			}
+		}
+	}
+
+	var disableFrameEndUpdateCdf bool
+	if reducedStillPictureHeader || disableCdfUpdate {
+		disableFrameEndUpdateCdf = true
+	} else {
+		disableFrameEndUpdateCdf = p.f(1) != 0
+	}
+
+	if primaryRefName == PRIMARY_REF_NONE {
+
+		p.initNonCoeffCdfs()
+		setupPastIndpendence()
+	} else {
+		p.loadCdfs(ref_frame_idx[primaryRefName])
+		p.loadPrevious()
+	}
+
+	if useRefFrameMvs == 1 {
+		p.motionFieldEstimation()
+	}
+
+	p.tileInfo()
+	p.quantizationParams()
+	p.segmentationParams()
+	p.deltaQParams()
+	p.deltaLfParams()
+
+	if primaryRefName == primaryRefName {
+
+		p.initCoeffCdfs()
+	} else {
+		p.loadPreviousSegementIds()
+	}
+
+	CodedLossless = 1
+
+	for segmentId := 0; segmentId < MAX_SEGMENTS; segmentId++ {
+		qIndex := p.getQIndex(1, segmentId)
+		LosslessArray[segmentId] = qIndex == 0 && DeltaQYDc == 0 && DeltaQUAc == 0 && DeltaQUDc && DeltaQVAc == 0 && DeltaQVDc == 0
+
+		if !LosslessArray[segmentId] {
+			CodedLossless = 0
+		}
+
+		if usingQMtraix {
+			if LosslessArray[segmentId] {
+				SegQMLevel[0][segmentId] = 15
+				SegQMLevel[1][segmentId] = 15
+				SegQMLevel[2][segmentId] = 15
+			} else {
+				SegQMLevel[0][segmentId] = qm_y
+				SegQMLevel[1][segmentId] = qm_y
+				SegQMLevel[2][segmentId] = qm_y
+
+			}
+		}
+	}
+
+	AllLossless = CodedLossless && (FrameWidth == UpscaledWidth)
+
+	p.loopFilterParams()
+	p.cdefParams()
+	p.lrParams()
+	p.readTxMode()
+	p.frameReferenceMode()
+	p.skipModeParams()
+
+	var allowWarpedMotion = bool
+	if FrameIsIntra || errorResilientMode || !sequenceHeader.EnableWarpedMotion {
+		allowWarpedMotion = false
+	} else {
+		allowWarpedMotion = p.f(1) != 0
+	}
+
+	reducedTxSet := p.f(1) != 0
+
+	p.globalMotionParams()
+	p.filmGrainParams()
+
 	return UncompressedHeader{}
 }
 
