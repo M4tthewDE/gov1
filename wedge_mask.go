b30f3bff87cbfa7e958f551ada9f47b322ef35a3
M4tthewDE <mattider2@gmail.com>
2023-02-26 22:07:31 +0100

    Implement 7.11.3.11 Wedge mask process

---
+++ wedge_mask.go
@@ -0,0 +1,147 @@
+package main
+
+const MASK_MASTER_SIZE = 64
+const WEDGE_TYPES = 16
+
+const WEDGE_HORIZONTAL = 0
+const WEDGE_VERTICAL = 1
+const WEDGE_OBLIQUE27 = 2
+const WEDGE_OBLIQUE63 = 3
+const WEDGE_OBLIQUE117 = 4
+const WEDGE_OBLIQUE153 = 5
+
+var Wedge_Bits = []int{0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0}
+
+var Wedge_Master_Oblique_Odd = []int{
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 6, 18,
+	37, 53, 60, 63, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+}
+
+var Wedge_Master_Oblique_Even = []int{
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 11, 27,
+	46, 58, 62, 63, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+}
+
+var Wedge_Master_Vertical = []int{
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 7, 21,
+	43, 57, 62, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+}
+
+var Wedge_Codebook = [][][]int{
+	{
+		{WEDGE_OBLIQUE27, 4, 4}, {WEDGE_OBLIQUE63, 4, 4},
+		{WEDGE_OBLIQUE117, 4, 4}, {WEDGE_OBLIQUE153, 4, 4},
+		{WEDGE_HORIZONTAL, 4, 2}, {WEDGE_HORIZONTAL, 4, 4},
+		{WEDGE_HORIZONTAL, 4, 6}, {WEDGE_VERTICAL, 4, 4},
+		{WEDGE_OBLIQUE27, 4, 2}, {WEDGE_OBLIQUE27, 4, 6},
+		{WEDGE_OBLIQUE153, 4, 2}, {WEDGE_OBLIQUE153, 4, 6},
+		{WEDGE_OBLIQUE63, 2, 4}, {WEDGE_OBLIQUE63, 6, 4},
+		{WEDGE_OBLIQUE117, 2, 4}, {WEDGE_OBLIQUE117, 6, 4},
+	},
+	{
+		{WEDGE_OBLIQUE27, 4, 4}, {WEDGE_OBLIQUE63, 4, 4},
+		{WEDGE_OBLIQUE117, 4, 4}, {WEDGE_OBLIQUE153, 4, 4},
+		{WEDGE_VERTICAL, 2, 4}, {WEDGE_VERTICAL, 4, 4},
+		{WEDGE_VERTICAL, 6, 4}, {WEDGE_HORIZONTAL, 4, 4},
+		{WEDGE_OBLIQUE27, 4, 2}, {WEDGE_OBLIQUE27, 4, 6},
+		{WEDGE_OBLIQUE153, 4, 2}, {WEDGE_OBLIQUE153, 4, 6},
+		{WEDGE_OBLIQUE63, 2, 4}, {WEDGE_OBLIQUE63, 6, 4},
+		{WEDGE_OBLIQUE117, 2, 4}, {WEDGE_OBLIQUE117, 6, 4},
+	},
+	{
+		{WEDGE_OBLIQUE27, 4, 4}, {WEDGE_OBLIQUE63, 4, 4},
+		{WEDGE_OBLIQUE117, 4, 4}, {WEDGE_OBLIQUE153, 4, 4},
+		{WEDGE_HORIZONTAL, 4, 2}, {WEDGE_HORIZONTAL, 4, 6},
+		{WEDGE_VERTICAL, 2, 4}, {WEDGE_VERTICAL, 6, 4},
+		{WEDGE_OBLIQUE27, 4, 2}, {WEDGE_OBLIQUE27, 4, 6},
+		{WEDGE_OBLIQUE153, 4, 2}, {WEDGE_OBLIQUE153, 4, 6},
+		{WEDGE_OBLIQUE63, 2, 4}, {WEDGE_OBLIQUE63, 6, 4},
+		{WEDGE_OBLIQUE117, 2, 4}, {WEDGE_OBLIQUE117, 6, 4},
+	},
+}
+
+var WedgeMasks = [][][][][]int{}
+
+func (t *TileGroup) InitialiseWedgeMaskTable(p *Parser) {
+	w := MASK_MASTER_SIZE
+	h := MASK_MASTER_SIZE
+
+	var MasterMask [][][]int
+	for j := 0; j < w; j++ {
+		shift := MASK_MASTER_SIZE / 4
+		for i := 0; i < h; i += 2 {
+			MasterMask[WEDGE_OBLIQUE63][i][j] = Wedge_Master_Oblique_Even[Clip3(0, MASK_MASTER_SIZE-1, j-shift)]
+			shift -= 1
+			MasterMask[WEDGE_OBLIQUE63][i+1][j] = Wedge_Master_Oblique_Odd[Clip3(0, MASK_MASTER_SIZE-1, j-shift)]
+			MasterMask[WEDGE_VERTICAL][i][j] = Wedge_Master_Vertical[j]
+			MasterMask[WEDGE_VERTICAL][i+1][j] = Wedge_Master_Vertical[j]
+		}
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			msk := MasterMask[WEDGE_OBLIQUE63][i][j]
+			MasterMask[WEDGE_OBLIQUE27][j][i] = msk
+			MasterMask[WEDGE_OBLIQUE117][i][w-1-j] = 64 - msk
+			MasterMask[WEDGE_OBLIQUE153][w-1-j][i] = 64 - msk
+			MasterMask[WEDGE_HORIZONTAL][j][i] = MasterMask[WEDGE_VERTICAL][i][j]
+		}
+	}
+
+	for bsize := BLOCK_8X8; bsize < BLOCK_SIZES; bsize++ {
+		if Wedge_Bits[bsize] > 0 {
+			w := t.Block_Width[bsize]
+			h := t.Block_Height[bsize]
+
+			for wedge := 0; wedge < WEDGE_TYPES; wedge++ {
+				dir := t.getWedgeDirection(bsize, wedge, p)
+				xoff := MASK_MASTER_SIZE/2 - ((t.getWedgeXoff(bsize, wedge, p) * w) >> 3)
+				yoff := MASK_MASTER_SIZE/2 - ((t.getWedgeYoff(bsize, wedge, p) * h) >> 3)
+				sum := 0
+				for i := 0; i < w; i++ {
+					sum += MasterMask[dir][yoff][xoff+i]
+				}
+				for i := 0; i < h; i++ {
+					sum += MasterMask[dir][yoff+i][xoff]
+				}
+				avg := (sum + (w+h-1)/2) / (w + h - 1)
+				flipSign := avg < 32
+				for i := 0; i < h; i++ {
+					for j := 0; j < w; j++ {
+						WedgeMasks[bsize][Int(flipSign)][wedge][i][j] = MasterMask[dir][yoff+i][xoff+j]
+						WedgeMasks[bsize][Int(!flipSign)][wedge][i][j] = 64 - MasterMask[dir][yoff+i][xoff+j]
+					}
+				}
+			}
+		}
+	}
+}
+
+func (t *TileGroup) getWedgeDirection(bsize int, index int, p *Parser) int {
+	return Wedge_Codebook[t.blockShape(bsize, p)][index][0]
+}
+
+func (t *TileGroup) getWedgeXoff(bsize int, index int, p *Parser) int {
+	return Wedge_Codebook[t.blockShape(bsize, p)][index][1]
+}
+
+func (t *TileGroup) getWedgeYoff(bsize int, index int, p *Parser) int {
+	return Wedge_Codebook[t.blockShape(bsize, p)][index][2]
+}
+
+func (t *TileGroup) blockShape(bsize int, p *Parser) int {
+	w4 := p.Num4x4BlocksWide[bsize]
+	h4 := p.Num4x4BlocksHigh[bsize]
+	if h4 > w4 {
+		return 0
+	} else if h4 < w4 {
+		return 1
+	}
+	return 2
+}
