6c3993bb4448562c7153d097ea4f13e089042ab0
M4tthewDE <mattider2@gmail.com>
2023-02-27 20:31:41 +0100

    Finish decode_partition( r, c, bSize )

--- tile_group.go
+++ tile_group.go
@@ -31,6 +31,12 @@ const PARTITION_NONE = 0
 const PARTITION_HORZ = 1
 const PARTITION_VERT = 2
 const PARTITION_SPLIT = 3
+const PARTITION_HORZ_A = 4
+const PARTITION_HORZ_B = 5
+const PARTITION_VERT_A = 6
+const PARTITION_VERT_B = 7
+const PARTITION_HORZ_4 = 8
+const PARTITION_VERT_4 = 9
 
 const MAX_SB_SIZE = 128
 const MAX_FRAME_DISTANCE = 31
@@ -897,6 +903,51 @@ func (t *TileGroup) decodePartition(r int, c int, bSize int, p *Parser) {
 	splitSize := Partition_Subsize[PARTITION_SPLIT][bSize]
 	if partition == PARTITION_NONE {
 		t.decodeBlock(r, c, subSize, p)
+	} else if partition == PARTITION_HORZ {
+		t.decodeBlock(r+halfBlock4x4, c, subSize, p)
+		if hasRows {
+			t.decodeBlock(r+halfBlock4x4, c, subSize, p)
+		}
+	} else if partition == PARTITION_VERT {
+		t.decodeBlock(r, c, subSize, p)
+		if hasCols {
+			t.decodeBlock(r, c+halfBlock4x4, subSize, p)
+		}
+	} else if partition == PARTITION_SPLIT {
+		t.decodePartition(r, c, subSize, p)
+		t.decodePartition(r, c+halfBlock4x4, subSize, p)
+		t.decodePartition(r+halfBlock4x4, c, subSize, p)
+		t.decodePartition(r+halfBlock4x4, c+halfBlock4x4, subSize, p)
+	} else if partition == PARTITION_HORZ_A {
+		t.decodeBlock(r, c, splitSize, p)
+		t.decodeBlock(r, c+halfBlock4x4, splitSize, p)
+		t.decodeBlock(r+halfBlock4x4, c, splitSize, p)
+	} else if partition == PARTITION_HORZ_B {
+		t.decodeBlock(r, c, subSize, p)
+		t.decodeBlock(r+halfBlock4x4, c, splitSize, p)
+		t.decodeBlock(r+halfBlock4x4, c+halfBlock4x4, splitSize, p)
+	} else if partition == PARTITION_VERT_A {
+		t.decodeBlock(r, c, splitSize, p)
+		t.decodeBlock(r+halfBlock4x4, c, splitSize, p)
+		t.decodeBlock(r, c+halfBlock4x4, subSize, p)
+	} else if partition == PARTITION_VERT_B {
+		t.decodeBlock(r, c, subSize, p)
+		t.decodeBlock(r, c+halfBlock4x4, splitSize, p)
+		t.decodeBlock(r+halfBlock4x4, c+halfBlock4x4, subSize, p)
+	} else if partition == PARTITION_HORZ_4 {
+		t.decodeBlock(r+quarterBlock4x4*0, c, subSize, p)
+		t.decodeBlock(r+quarterBlock4x4*1, c, subSize, p)
+		t.decodeBlock(r+quarterBlock4x4*2, c, subSize, p)
+		if r+quarterBlock4x4*3 < p.MiRows {
+			t.decodeBlock(r+quarterBlock4x4*3, c, subSize, p)
+		}
+	} else {
+		t.decodeBlock(r, c+quarterBlock4x4*0, subSize, p)
+		t.decodeBlock(r, c+quarterBlock4x4*1, subSize, p)
+		t.decodeBlock(r, c+quarterBlock4x4*2, subSize, p)
+		if c+quarterBlock4x4*3 < p.MiRows {
+			t.decodeBlock(r, c+quarterBlock4x4*3, subSize, p)
+		}
 	}
 }
 
@@ -918,19 +969,19 @@ func (t *TileGroup) decodeBlock(r int, c int, subSize int, p *Parser) {
 
 	p.AvailU = p.isInside(r-1, c)
 	p.AvailL = p.isInside(r, c-1)
-	availUChroma := p.AvailU
-	availLChroma := p.AvailL
+	p.AvailUChroma = p.AvailU
+	p.AvailLChroma = p.AvailL
 
 	if t.HasChroma {
 		if p.sequenceHeader.ColorConfig.SubsamplingY && bh4 == 1 {
-			availUChroma = p.isInside(r-2, c)
+			p.AvailUChroma = p.isInside(r-2, c)
 		}
 		if p.sequenceHeader.ColorConfig.SubsamplingX && bw4 == 1 {
-			availLChroma = p.isInside(r, c-2)
+			p.AvailLChroma = p.isInside(r, c-2)
 		}
 	} else {
-		availUChroma = false
-		availLChroma = false
+		p.AvailUChroma = false
+		p.AvailLChroma = false
 	}
 
 	t.modeInfo(p)
@@ -1016,8 +1067,8 @@ func (t *TileGroup) computePrediction(p *Parser) {
 			haveLeft := p.AvailLChroma
 			haveAbove := p.AvailUChroma
 			if plane == 0 {
-				haveLeft := p.AvailL
-				haveAbove := p.AvailU
+				haveLeft = p.AvailL
+				haveAbove = p.AvailU
 			}
 			t.predictIntra(plane, baseX, baseY, haveLeft, haveAbove, p.BlockDecoded[plane][(subBlockMiRow>>subY)-1][(subBlockMiCol>>subX)+num4x4W], p.BlockDecoded[plane][(subBlockMiRow>>subY)+num4x4H][(subBlockMiCol>>subX)-1], mode, log2W, log2H, p)
 		}
