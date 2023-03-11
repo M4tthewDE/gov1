5b76c867f01e8b4ec82bc34c3de3b81c20c7a982
M4tthewDE <mattider2@gmail.com>
2023-03-11 19:16:33 +0100

    "Fix" Num4x4BlocksHigh size

--- internal/state/state.go
+++ internal/state/state.go
@@ -6,31 +6,33 @@ import (
 )
 
 type State struct {
-	MiCols                 int
-	MiRows                 int
-	TileColsLog2           int
-	TileRowsLog2           int
-	MiRowStarts            []int
-	MiColStarts            []int
-	TileCols               int
-	TileRows               int
-	TileSizeBytes          int
-	FeatureEnabled         [shared.MAX_SEGMENTS][shared.SEG_LVL_MAX]int
-	FeatureData            [shared.MAX_SEGMENTS][shared.SEG_LVL_MAX]int
-	GmType                 [shared.ALTREF_FRAME + 1]int
-	PrevGmParams           [shared.ALTREF_FRAME + 1][6]int
-	PrevSegmentIds         [][]int
-	RefFrameType           [7]int
-	CurrentQIndex          int
-	OperatingPointIdc      int
-	MiColStart             int
-	MiColEnd               int
-	MiRowStart             int
-	MiRowEnd               int
-	BlockWidth             []int
-	BlockHeight            []int
+	MiCols            int
+	MiRows            int
+	TileColsLog2      int
+	TileRowsLog2      int
+	MiRowStarts       []int
+	MiColStarts       []int
+	TileCols          int
+	TileRows          int
+	TileSizeBytes     int
+	FeatureEnabled    [shared.MAX_SEGMENTS][shared.SEG_LVL_MAX]int
+	FeatureData       [shared.MAX_SEGMENTS][shared.SEG_LVL_MAX]int
+	GmType            [shared.ALTREF_FRAME + 1]int
+	PrevGmParams      [shared.ALTREF_FRAME + 1][6]int
+	PrevSegmentIds    [][]int
+	RefFrameType      [7]int
+	CurrentQIndex     int
+	OperatingPointIdc int
+	MiColStart        int
+	MiColEnd          int
+	MiRowStart        int
+	MiRowEnd          int
+	BlockWidth        []int
+	BlockHeight       []int
+
+	// this size is just a guess pretty much
 	Num4x4BlocksWide       [shared.BLOCK_128X128]int
-	Num4x4BlocksHigh       []int
+	Num4x4BlocksHigh       [shared.BLOCK_128X128]int
 	SeenFrameHeader        bool
 	RenderWidth            int
 	RenderHeight           int
