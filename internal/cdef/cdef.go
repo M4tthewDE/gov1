df1c274f4357feeeda0490efc998db2f0d578754
M4tthewDE <mattider2@gmail.com>
2023-03-11 19:14:41 +0100

    Increase cdef array size dynamically

--- internal/cdef/cdef.go
+++ internal/cdef/cdef.go
@@ -6,6 +6,14 @@ type Cdef struct {
 }
 
 func (cdef *Cdef) ClearCdef(r int, c int, Use128x128SuperBlock bool, cdefSize4 int) {
+	for cap(cdef.CdefIdx) < r+1 {
+		cdef.CdefIdx = append(cdef.CdefIdx, make([]int, 0))
+	}
+
+	for cap(cdef.CdefIdx[r]) < c+1 {
+		cdef.CdefIdx[r] = append(cdef.CdefIdx[r], 0)
+	}
+
 	cdef.CdefIdx[r][c] = -1
 
 	if Use128x128SuperBlock {
