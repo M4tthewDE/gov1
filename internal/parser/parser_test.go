ffe0caf1edac44f5a5048e1d8119740df406cae9
M4tthewDE <mattider2@gmail.com>
2023-04-01 11:11:15 +0200

    Generate out.yuv file in test

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"bytes"
+	"encoding/binary"
 	"os"
 	"testing"
 
@@ -9,6 +11,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func TestParseEndToEnd(t *testing.T) {
 	if testing.Short() {
@@ -26,9 +34,34 @@ func TestParseEndToEnd(t *testing.T) {
 	obus := p.bitstream()
 	logger.Logger.Info("DONE ", zap.Int("obu count", len(obus)))
 
-	obu := obus[2]
+	obu := obus[len(obus)-1]
 
-	assert.Equal(t, true, obu.SequenceHeader.ColorConfig.SubsamplingX)
-	assert.Equal(t, true, obu.SequenceHeader.ColorConfig.SubsamplingY)
-	assert.Equal(t, false, obu.SequenceHeader.ColorConfig.MonoChrome)
+	buf := new(bytes.Buffer)
+	for _, yArr := range obu.TileGroup.OutY {
+		for _, y := range yArr {
+			binary.Write(buf, binary.BigEndian, y)
+		}
+	}
+
+	for _, uArr := range obu.TileGroup.OutU {
+		for _, u := range uArr {
+			binary.Write(buf, binary.BigEndian, u)
+		}
+	}
+
+	for _, vArr := range obu.TileGroup.OutV {
+		for _, v := range vArr {
+			binary.Write(buf, binary.BigEndian, v)
+		}
+	}
+
+	f, err := os.Create("../../out.yuv")
+	check(err)
+
+	defer f.Close()
+
+	n, err := f.Write(buf.Bytes())
+	check(err)
+
+	assert.NotEqual(t, 0, n)
 }
